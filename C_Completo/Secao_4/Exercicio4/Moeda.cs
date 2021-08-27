edbebfa188fd265f071f92b0a91e57e62b9192e3
Wanderlei Ribeiro Dias <wanderlei.ribeiro@gmail.com>
2021-08-27 18:18:00 -0300

    Iniciando seção 5

---
+++ C_Completo/Secao_4/Exercicio4/Moeda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4
+{
+    static class Moeda
+    {
+        public static double Iof = 6;
+
+        public static double ConversaoReal(double cotacao, double qtdeDolar)
+        {
+            return qtdeDolar * (1 + (Iof / 100)) * cotacao;
+        }
+    }
+}
---
+++ C_Completo/Secao_4/Exercicio4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cot = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            double vdolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Qual é a cotação do dólar? "+ Moeda.ConversaoReal(cot, vdolares).ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
