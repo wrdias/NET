e94b5c951249678a14d602631be33c697dabe8ea
Wanderlei Ribeiro Dias <wanderlei.ribeiro@gmail.com>
2021-09-08 17:37:47 -0300

    Curso ASP.NET Core 3 - Desenvolvendo a primeira classe de teste

--- ASP_NET_Core3_Restful/Secao_3/RallyDakar/RallyDakar.Dominio/Entidades/Temporada.cs
+++ ASP_NET_Core3_Restful/Secao_3/RallyDakar/RallyDakar.Dominio/Entidades/Temporada.cs
@@ -9,10 +9,22 @@ namespace RallyDakar.Dominio.Entidades
     public class Temporada
     {
         public int Id { get; set; }
-        public int Nome { get; set; }
+        public string Nome { get; set; }
         public DateTime DataInicio { get; set; }
         public DateTime? DataFim { get; set; }
 
-        public ICollection<Equipe> Equipes { get; set; }
+        public virtual ICollection<Equipe> Equipes { get; set; }
+
+        public Temporada()
+        {
+            Equipes = new List<Equipe>();
+        }
+
+        public void AdicionarEquipe(Equipe equipe)
+        {
+            if (equipe != null)
+                if (!String.IsNullOrEmpty(equipe.Nome))
+                    Equipes.Add(equipe);
+        }
     }
 }
